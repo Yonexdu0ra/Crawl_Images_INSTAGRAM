bebafbcc3095ac4405cd75063bfad51dc75e7167
Yonexdu0ra <nguoidungemail1@gmail.com>
2022-12-02 22:02:45 +0700

    Unfinished function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
 const routes = require('./routes')
-const comprestion = require('compression')
-// const puppeteer = require('puppeteer-core');
 const { engine } = require('express-handlebars')
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000
 // config
-app.use(comprestion());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.engine('.hbs', engine({ extname: '.hbs'}));
